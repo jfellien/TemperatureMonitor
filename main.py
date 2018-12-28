a664679942c1d9a990f6f323293a9c0e2b4c2109
Janek <janekf@Janeks-MBP.fritz.box>
2018-12-28 21:29:33 +0100

    boring

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def show_temperature(temperature):
 
         epd.update_screen()
 
-        print title
-        print temperature_value)
+        print(title)
+        print(temperature_value)
 
 def get_current_temperature():
         file = open('/sys/bus/w1/devices/28-01d54c07010c/w1_slave')
