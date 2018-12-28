b7453eb95421b46fe06f21a45caa7cbcec6314d5
Janek <janekf@Janeks-MBP.fritz.box>
2018-12-28 19:49:24 +0100

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+from PIL import ImageFont
+
+import EPD.epsimplelib
+
+FONT_PATH      = './font/FreeMonoBold.ttf'
+FONT_BIG       = ImageFont.truetype(FONT_PATH, 72)
+
+BLACK = 0
+
+eps = epsimplelib.EPScreen('landscape')
+
+def show_temperature(temperature):
+        eps.set_title("Aktuelle Temperatur")
+
+        eps.add_text_middle(60, str(temperature) + " °C", FONT_BIG, BLACK)
+
+        eps.update_screen()
+
+def get_current_temperature():
+        return 45
+
+temperature = get_current_temperature()
+show_temperature(temperature)
